31af1276c3be26296d1042801c4ab668c08b8a2b
E Thomas <ethomas997@etsphere.com>
2019-08-19 22:48:59 -0400

    Added startup and radio-failsafe sounds to Buzzer tones

--- libraries/AP_Notify/Buzzer.cpp
+++ libraries/AP_Notify/Buzzer.cpp
@@ -22,8 +22,13 @@
 #include "AP_Notify.h"
 
 #ifndef HAL_BUZZER_ON
- #define HAL_BUZZER_ON 1
- #define HAL_BUZZER_OFF 0 
+  #if !defined(HAL_BUZZER_PIN)
+    #define HAL_BUZZER_ON (pNotify->get_buzz_level())
+    #define HAL_BUZZER_OFF (!pNotify->get_buzz_level())
+  #else
+    #define HAL_BUZZER_ON 1
+    #define HAL_BUZZER_OFF 0 
+  #endif
 #endif
 
 
@@ -57,117 +62,64 @@ bool Buzzer::init()
 // update - updates led according to timed_updated.  Should be called at 50Hz
 void Buzzer::update()
 {
+    update_pattern_to_play();
+    update_playing_pattern();
+}
+
+void Buzzer::update_pattern_to_play()
+{
+    if (!_flags.initialize_done) {
+        // play a quick beep at startup
+        if (!_flags.startup_beep) {
+            _flags.startup_beep = true;
+            play_pattern(SINGLE_BUZZ);
+            return;
+        }
+        // play beeps when system initialization complete
+        if (!_flags.initialize_started) {
+            if (AP_Notify::flags.initialising) {
+                _flags.initialize_started = true;
+            }
+        } else if (!AP_Notify::flags.initialising) {
+            _flags.initialize_done = true;
+            play_pattern(INITIALIZE_BUZZ);
+            return;
+        }
+    }
+
+    // check if radio failsafe status has changed
+    if (_flags.failsafe_radio != AP_Notify::flags.failsafe_radio) {
+        _flags.failsafe_radio = AP_Notify::flags.failsafe_radio;
+        // play beeps to indicate radio status (if initialization completed)
+        if (_flags.initialize_done) {
+            play_pattern(_flags.failsafe_radio ? RADIOLOST_BUZZ : RADIOBACK_BUZZ);
+            return;
+        }
+    }
+
     // check for arming failed event
     if (AP_Notify::events.arming_failed) {
         // arming failed buzz
         play_pattern(SINGLE_BUZZ);
-    }
-
-    // reduce 50hz call down to 10hz
-    _counter++;
-    if (_counter < 5) {
         return;
     }
-    _counter = 0;
 
-    // complete currently played pattern
-    if (_pattern != NONE) {
-        _pattern_counter++;
-        switch (_pattern) {
-            case SINGLE_BUZZ:
-                // buzz for 10th of a second
-                if (_pattern_counter == 1) {
-                    on(true);
-                }else{
-                    on(false);
-                    _pattern = NONE;
-                }
-                return;
-            case DOUBLE_BUZZ:
-                // buzz for 10th of a second
-                switch (_pattern_counter) {
-                    case 1:
-                        on(true);
-                        break;
-                    case 2:
-                        on(false);
-                        break;
-                    case 3:
-                        on(true);
-                        break;
-                    case 4:
-                    default:
-                        on(false);
-                        _pattern = NONE;
-                        break;
-                }
-                return;
-            case ARMING_BUZZ:
-                // record start time
-                if (_pattern_counter == 1) {
-                    _arming_buzz_start_ms = AP_HAL::millis();
-                    on(true);
-                } else {
-                    // turn off buzzer after 3 seconds
-                    if (AP_HAL::millis() - _arming_buzz_start_ms >= BUZZER_ARMING_BUZZ_MS) {
-                        _arming_buzz_start_ms = 0;
-                        on(false);
-                        _pattern = NONE;
-                    }
-                }
-                return;
-            case BARO_GLITCH:
-                // four fast tones
-                switch (_pattern_counter) {
-                    case 1:
-                    case 3:
-                    case 5:
-                    case 7:
-                    case 9:
-                        on(true);
-                        break;
-                    case 2:
-                    case 4:
-                    case 6:
-                    case 8:
-                        on(false);
-                        break;
-                    case 10:
-                        on(false);
-                        _pattern = NONE;
-                        break;
-                    default:
-                        // do nothing
-                        break;
-                }
-                return;
-            case EKF_BAD:
-                // four tones getting shorter)
-                switch (_pattern_counter) {
-                    case 1:
-                    case 5:
-                    case 8:
-                    case 10:
-                        on(true);
-                        break;
-                    case 4:
-                    case 7:
-                    case 9:
-                        on(false);
-                        break;
-                    case 11:
-                        on(false);
-                        _pattern = NONE;
-                        break;
-                    default:
-                        // do nothing
-                        break;
-                }
-                return;
-            default:
-                // do nothing
-                break;
-        }
+    // do not interrupt playing patterns
+    if (is_playing_pattern()) {
+        return;
+    }
+
+    // if battery failsafe constantly buzz
+    if (AP_Notify::flags.failsafe_battery) {
+        play_pattern(CONSTANT_BUZZ);
+        return;
+    }
+
+    // if radio failsafe active and system was (ever) armed then
+    // repeat SOS beeps until radio recovered (for lost model locator)
+    if (_flags.failsafe_radio && _flags.was_armed) {
+        play_pattern(RADIOSOS_BUZZ);
+        return;
     }
 
     // check if armed status has changed
@@ -176,6 +128,7 @@ void Buzzer::update()
         if (_flags.armed) {
             // double buzz when armed
             play_pattern(ARMING_BUZZ);
+            _flags.was_armed = true;
         }else{
             // single buzz when disarmed
             play_pattern(SINGLE_BUZZ);
@@ -189,19 +142,46 @@ void Buzzer::update()
         if (_flags.ekf_bad) {
             // ekf bad warning buzz
             play_pattern(EKF_BAD);
+            return;
         }
-        return;
     }
 
     // if vehicle lost was enabled, starting beep
     if (AP_Notify::flags.vehicle_lost) {
         play_pattern(DOUBLE_BUZZ);
+        return;
+    }
+}
+
+
+void Buzzer::update_playing_pattern()
+{
+    // if no '1's to play in pattern and buzzer not on then just return
+    if (_pattern == 0UL && !_flags.on) {
+        return;
     }
 
-    // if battery failsafe constantly single buzz
-    if (AP_Notify::flags.failsafe_battery) {
-        play_pattern(SINGLE_BUZZ);
+    // reduce 50hz call down to 10hz
+    if (--_update_counter > 0) {
+        return;
     }
+    _update_counter = 5;
+
+    // put buzzer on if leftmost bit set, and then shift one bit to the left
+    const bool on_flag = _pattern & (0b10000000000000000000000000000000UL);
+    _pattern <<= 1;
+    ++_pattern_pos;
+    
+    // can use trailing '...001' in pattern to extend timing, so
+    // don't turn buzzer on if it's off when that last '1' is reached
+    if (_pattern_pos != (uint8_t)32 || _pattern != 0UL || _flags.on) {
+        on(on_flag);
+    }
+}
+
+bool Buzzer::is_playing_pattern()
+{
+    return (_pattern != 0UL || _flags.on);
 }
 
 // on - turns the buzzer on or off
@@ -220,9 +200,10 @@ void Buzzer::on(bool turn_on)
 }
 
 /// play_pattern - plays the defined buzzer pattern
-void Buzzer::play_pattern(BuzzerPattern pattern_id)
+void Buzzer::play_pattern(const uint32_t pattern)
 {
-    _pattern = pattern_id;
-    _pattern_counter = 0;
+    _pattern = pattern;
+    _update_counter = 0;  // init counter for no initial delay
+    _pattern_pos = 0;
 }
 
--- libraries/AP_Notify/Buzzer.h
+++ libraries/AP_Notify/Buzzer.h
@@ -19,8 +19,6 @@
 
 #include <AP_HAL/AP_HAL.h>
 
-#define BUZZER_ARMING_BUZZ_MS   3000    // arming buzz length in milliseconds (i.e. 3 seconds)
-
 #include "NotifyDevice.h"
 
 class Buzzer: public NotifyDevice
@@ -30,29 +28,31 @@ public:
     Buzzer() {}
 
     /// init - initialise the buzzer
-    bool init(void);
+    bool init(void) override;
 
     /// update - updates buzzer according to timed_updated.  Should be called at 50Hz
-    void update();
+    void update() override;
+
+private:
 
     /// on - turns the buzzer on or off
     void on(bool on_off);
 
-    // Patterns - how many beeps will be played
-    enum BuzzerPattern {
-        NONE = 0,
-        SINGLE_BUZZ = 1,
-        DOUBLE_BUZZ = 2,
-        GPS_GLITCH = 3, // not used
-        ARMING_BUZZ = 4,
-        BARO_GLITCH = 5,
-        EKF_BAD = 6
-    };
+    // Patterns - how many beeps will be played; read from
+    // left-to-right, each bit represents 100ms
+    static const uint32_t     SINGLE_BUZZ = 0b10000000000000000000000000000000UL;
+    static const uint32_t     DOUBLE_BUZZ = 0b10100000000101000000010100000001UL;
+    static const uint32_t     ARMING_BUZZ = 0b11111111111111111111111111111100UL; // 3s
+    static const uint32_t   CONSTANT_BUZZ = 0b11111111111111111111111111111111UL;
+    static const uint32_t       BARO_BUZZ = 0b10101010100000000000000000000000UL;
+    static const uint32_t         EKF_BAD = 0b11101101010000000000000000000000UL;
+    static const uint32_t INITIALIZE_BUZZ = 0b10101000000000000000000000000000UL;
+    static const uint32_t  RADIOLOST_BUZZ = 0b11011111110000000000000000000000UL;
+    static const uint32_t   RADIOSOS_BUZZ = 0b00010101011101110111010101000001UL; // SOS pattern
+    static const uint32_t  RADIOBACK_BUZZ = 0b01111010000000000000000000000000UL;
 
     /// play_pattern - plays the defined buzzer pattern
-    void play_pattern(BuzzerPattern pattern_id);
-
-private:
+    void play_pattern(const uint32_t pattern);
 
     /// buzzer_flag_type - bitmask of current state and ap_notify states we track
     struct buzzer_flag_type {
@@ -61,11 +61,20 @@ private:
         uint8_t armed               : 1;    // 0 = disarmed, 1 = armed
         uint8_t failsafe_battery    : 1;    // 1 if battery failsafe has triggered
         uint8_t ekf_bad             : 1;    // 1 if ekf position has gone bad
+        uint8_t initialize_started  : 1;    // 1 if system initialization started
+        uint8_t initialize_done     : 1;    // 1 if system initialization complete
+        uint8_t startup_beep        : 1;    // 1 after startup single-beep played
+        uint8_t failsafe_radio      : 1;    // 1 if radio failsafe has triggered
+        uint8_t was_armed           : 1;    // 1 if system has been armed
     } _flags;
 
-    uint8_t         _counter;           // reduces 50hz update down to 10hz for internal processing
-    BuzzerPattern   _pattern;           // current pattern
-    uint8_t         _pattern_counter;   // used to time on/off of current patter
-    uint32_t        _arming_buzz_start_ms;  // arming_buzz start time in milliseconds
-    uint8_t         _pin;
+    int8_t _update_counter;      // reduce 50hz update down to 10hz for internal processing
+    uint32_t _pattern;           // current pattern
+    uint8_t _pattern_pos;        // current position in pattern
+    uint8_t _pin;
+
+    void update_playing_pattern();
+    void update_pattern_to_play();
+    bool is_playing_pattern();
+
 };
