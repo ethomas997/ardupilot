76f313e01aa468aa26381cddad2ea7a376721776
E Thomas <ethomas997@etsphere.com>
2019-09-28 23:42:14 -0400

    Updated version code to 'V3.9.11 etFenceGPSbuz'

--- ArduPlane/version.h
+++ ArduPlane/version.h
@@ -6,15 +6,15 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "ArduPlane V3.9.11"
+#define THISFIRMWARE "ArduPlane V3.9.11 etFenceGPSbuz"
 
 // the following line is parsed by the autotest scripts
-#define FIRMWARE_VERSION 3,9,11,FIRMWARE_VERSION_TYPE_OFFICIAL
+#define FIRMWARE_VERSION 3,9,11,FIRMWARE_VERSION_TYPE_DEV
 
 #define FW_MAJOR 3
 #define FW_MINOR 9
 #define FW_PATCH 11
-#define FW_TYPE FIRMWARE_VERSION_TYPE_OFFICIAL
+#define FW_TYPE FIRMWARE_VERSION_TYPE_DEV
 
 
 
